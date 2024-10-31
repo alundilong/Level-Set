0882ca085edbcf7bb1311d7e54d23be8532fbcf0
alundilong <ymao.mu@gmail.com>
2024-10-31 12:24:10 -0500

    refactor: comment out saving intermediate images

--- lv_set/lv_set/drlse_algo_3d.py
+++ lv_set/lv_set/drlse_algo_3d.py
@@ -374,10 +374,10 @@ def drlse_edge_gpu(phi_0, g, lmda, mu, alfa, epsilon, timestep, iters, potential
         area_term = dirac_phi * g
         edge_term = dirac_phi * (vx * n_x + vy * n_y + vz * n_z) + dirac_phi * g * curvature
         phi += timestep * (mu * dist_reg_term + lmda * edge_term + alfa * area_term)
-        dump_image_to_vtk(phi.cpu().numpy(),f"edge_innerloop_{drlse_edge.call_count}.vti")
-        dump_image_to_vtk(area_term.cpu().numpy(),f"edge_area_term_{drlse_edge.call_count}.vti")
-        dump_image_to_vtk(edge_term.cpu().numpy(),f"edge_edge_term_{drlse_edge.call_count}.vti")
-        dump_image_to_vtk(dist_reg_term.cpu().numpy(),f"edge_dist_reg_term_{drlse_edge.call_count}.vti")
+        # dump_image_to_vtk(phi.cpu().numpy(),f"edge_innerloop_{drlse_edge.call_count}.vti")
+        # dump_image_to_vtk(area_term.cpu().numpy(),f"edge_area_term_{drlse_edge.call_count}.vti")
+        # dump_image_to_vtk(edge_term.cpu().numpy(),f"edge_edge_term_{drlse_edge.call_count}.vti")
+        # dump_image_to_vtk(dist_reg_term.cpu().numpy(),f"edge_dist_reg_term_{drlse_edge.call_count}.vti")
     return phi.cpu().numpy()  # Move result back to CPU for further processing if needed
 
 def drlse_threshold_gpu(phi_0, img, lmda, mu, alfa, epsilon, upper, lower, timestep, iters, potential_function, device="cuda"):
@@ -413,10 +413,10 @@ def drlse_threshold_gpu(phi_0, img, lmda, mu, alfa, epsilon, upper, lower, times
         area_term = (eps - torch.abs(img - T)) / eps * dirac_phi * 80.0
         edge_term = curvature * dirac_phi
         phi += timestep * 0.2 * (mu * dist_reg_term + lmda * edge_term + alfa * area_term)
-        dump_image_to_vtk(phi.cpu().numpy(),f"threshold_innerloop_{drlse_edge.call_count}.vti")
-        dump_image_to_vtk(area_term.cpu().numpy(),f"threshold_area_term_{drlse_edge.call_count}.vti")
-        dump_image_to_vtk(edge_term.cpu().numpy(),f"threshold_edge_term_{drlse_edge.call_count}.vti")
-        dump_image_to_vtk(dist_reg_term.cpu().numpy(),f"threshold_dist_reg_term_{drlse_edge.call_count}.vti")
+        # dump_image_to_vtk(phi.cpu().numpy(),f"threshold_innerloop_{drlse_edge.call_count}.vti")
+        # dump_image_to_vtk(area_term.cpu().numpy(),f"threshold_area_term_{drlse_edge.call_count}.vti")
+        # dump_image_to_vtk(edge_term.cpu().numpy(),f"threshold_edge_term_{drlse_edge.call_count}.vti")
+        # dump_image_to_vtk(dist_reg_term.cpu().numpy(),f"threshold_dist_reg_term_{drlse_edge.call_count}.vti")
     return phi.cpu().numpy()
 
 def laplace_gpu(input):
